0528986f4bce0350fa59fb16b9a77982584f1517
dkorenci <damir.korencic@gmail.com>
2017-06-09 17:03:25 +0200

    added reformatting of dataset for CNN

--- 01 from DL to NN/notmnist/dataset_reformat.py
+++ 01 from DL to NN/notmnist/dataset_reformat.py
@@ -7,6 +7,7 @@ import numpy as np
 def reformat1d(dataset, labels):
   '''
   Flatten last two dimensions (holding image pixels) from 2d array to 1d array
+  Turn lables from integers to 1-hot encoding.
   :return:
   '''
   dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
@@ -14,17 +15,26 @@ def reformat1d(dataset, labels):
   labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
   return dataset, labels
 
-def reformatted1Ddataset(ds):
+def reformatDataset(ds, reformatter):
     new = {}
-    new['train_dataset'], new['train_labels'] = reformat1d(ds['train_dataset'], ds['train_labels'])
-    new['valid_dataset'], new['valid_labels'] = reformat1d(ds['valid_dataset'], ds['valid_labels'])
-    new['test_dataset'], new['test_labels'] = reformat1d(ds['test_dataset'], ds['test_labels'])
+    new['train_dataset'], new['train_labels'] = reformatter(ds['train_dataset'], ds['train_labels'])
+    new['valid_dataset'], new['valid_labels'] = reformatter(ds['valid_dataset'], ds['valid_labels'])
+    new['test_dataset'], new['test_labels'] = reformatter(ds['test_dataset'], ds['test_labels'])
     return new
 
+def reformatConv(dataset, labels, image_size = 28, num_labels = 10, num_channels = 1):
+    '''
+    Reformat MNIST dataset for convolutional networks, turning images to 3d
+        arrays by adding additional 'channel' dimension.
+    '''
+    dataset = dataset.reshape((-1, image_size, image_size, num_channels)).astype(np.float32)
+    labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+    return dataset, labels
+
 if __name__ == '__main__':
-    #ds = loadDataset()
-    ds = loadDataset('notMNIST_reformatted_1d_images.pickle')
-    printDsetShapes(ds)
-    #nds = reformattedDataset(ds)
-    #printDsetShapes(nds)
-    #saveDataset(nds, 'notMNIST_reformatted.pickle')
+    ds = loadDataset()
+    #ds = loadDataset('notMNIST_reformatted_1d_images.pickle')
+    #printDsetShapes(ds)
+    # nds = reformatDataset(ds, reformatConv)
+    # printDsetShapes(nds)
+    # saveDataset(nds, 'notMNIST_reformatted_conv.pickle')
