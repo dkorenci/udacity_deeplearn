f72a6492aac6a0d9484852bbc41b554b9d5c0e28
dkorenci <damir.korencic@gmail.com>
2017-11-14 16:41:25 +0100

    added coding LSTM to the model

--- 05 Deep Models for Texts/text_inverter/model.py
+++ 05 Deep Models for Texts/text_inverter/model.py
@@ -1,10 +1,22 @@
 import tensorflow as tf
+from tensorflow.contrib.rnn import BasicLSTMCell
+from tensorflow.contrib.rnn import DropoutWrapper
+from tensorflow.contrib.rnn import MultiRNNCell
 
 class InvertingLSTM():
 
-    def __init__(self, seqSize, depth, alphabetSize, batchSize):
+    def __init__(self, seqSize, alphabetSize, batchSize, networkSize, depth=1, dropout=0.5):
+        '''
+        :param seqSize: lenght of sequences the net will be trained on
+        :param depth: depth of LSTM, number of stacked cells
+        :param alphabetSize: equiv. to the size of the char vector
+        :param batchSize:
+        :param networkSize: num. hidden units in the network
+        :param dropout: rate of the dropout
+        '''
         self.seqSize, self.depth = seqSize, depth
         self.alphabetSize, self.batchSize = alphabetSize, batchSize
+        self.networkSize = networkSize; self.dropout = dropout
         self.__buildInputs()
         self.__buildNetwork()
         self.__buildLoss()
@@ -20,28 +32,55 @@ class InvertingLSTM():
         with tf.name_scope('correct_output'):
             self.correctOutBatch = tf.reverse(self.inputBatch, [1], name='correct_output')
 
+    def __buildNetwork(self):
+        ccell = self.__buildCodingCell()
+        initState = ccell.zero_state(self.batchSize, tf.float32)
+        self.cout, self.cstate = \
+            tf.nn.dynamic_rnn(ccell, self.inputBatch, initial_state=initState)
+        dcell = self.__buildDecodingLSTM()
+
+    def __buildCodingCell(self):
+        def cell():
+            c = BasicLSTMCell(num_units=self.networkSize)
+            c = DropoutWrapper(c, output_keep_prob=self.dropout)
+            return c
+        if self.depth > 1: cell = MultiRNNCell([cell() for _ in range(self.depth)])
+        else: cell = cell()
+        return cell
+
+    def __buildDecodingLSTM(self): pass
+
+
     def evalInputs(self, inBatch):
         with tf.Session() as sess:
-            inp, out = sess.run([self.inputBatch, self.correctOutBatch], feed_dict={self.rawBatch:inBatch})
-            print(type(inp))
-            print(inp)
-            print(type(out))
-            print(out)
+            inp, out = sess.run([self.inputBatch, self.correctOutBatch], feed_dict={self.rawBatch: inBatch})
+            printt(inp); printt(out)
 
-    def __buildNetwork(self): pass
+    def evalCoding(self, inBatch):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            cout, cstate = sess.run([self.cout, self.cstate],
+                                    feed_dict={self.rawBatch: inBatch})
+            printt(cout); printt(cstate)
 
     def __buildLoss(self): pass
 
     def __buildOptimizer(self): pass
 
+def printt(t):
+    ''' Print tensor '''
+    print(type(t))
+    print(t)
+
 def test():
-    model = InvertingLSTM(4, 2, 3, 3)
+    model = InvertingLSTM(4, 3, 3, 5)
     batch = [
         [ 0, 2, 1, 1 ],
         [ 1, 2, 0, 2 ],
         [ 2, 0, 1, 0 ]
     ]
     model.evalInputs(batch)
+    model.evalCoding(batch)
 
 if __name__ == '__main__':
     test()
