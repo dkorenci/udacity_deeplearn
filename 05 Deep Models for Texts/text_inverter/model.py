e03ed505ed89f837b2062681f81d322598fba698
dkorenci <damir.korencic@gmail.com>
2017-11-16 20:55:29 +0100

    created decoding network

--- 05 Deep Models for Texts/text_inverter/model.py
+++ 05 Deep Models for Texts/text_inverter/model.py
@@ -33,13 +33,21 @@ class InvertingLSTM():
             self.correctOutBatch = tf.reverse(self.inputBatch, [1], name='correct_output')
 
     def __buildNetwork(self):
-        ccell = self.__buildCodingCell()
-        initState = ccell.zero_state(self.batchSize, tf.float32)
-        self.cout, self.cstate = \
-            tf.nn.dynamic_rnn(ccell, self.inputBatch, initial_state=initState)
-        dcell = self.__buildDecodingLSTM()
+        with tf.name_scope('coding_network'), tf.variable_scope('coding'):
+            #with tf.variable_scope("model", reuse=True):
+            ccell = self.__buildLSTMCell()
+            initState = ccell.zero_state(self.batchSize, tf.float32)
+            self.cout, self.cstate = \
+                tf.nn.dynamic_rnn(ccell, self.inputBatch, initial_state=initState)
+        with tf.name_scope('decoding_network'), tf.variable_scope('decoding'):
+            dcell = self.__buildLSTMCell()
+            # all zeros for now, but it can be passed self.cout as the first element
+            self.decodeInput = tf.zeros((self.batchSize, self.seqSize, self.alphabetSize),
+                                        dtype=tf.float32)
+            self.dout, self.dstate = \
+                tf.nn.dynamic_rnn(dcell, self.decodeInput, initial_state=self.cstate)
 
-    def __buildCodingCell(self):
+    def __buildLSTMCell(self):
         def cell():
             c = BasicLSTMCell(num_units=self.networkSize)
             c = DropoutWrapper(c, output_keep_prob=self.dropout)
@@ -56,12 +64,15 @@ class InvertingLSTM():
             inp, out = sess.run([self.inputBatch, self.correctOutBatch], feed_dict={self.rawBatch: inBatch})
             printt(inp); printt(out)
 
-    def evalCoding(self, inBatch):
+    def evalNetwork(self, inBatch):
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            cout, cstate = sess.run([self.cout, self.cstate],
+            cout, cstate, dout, dstate = sess.run([self.cout, self.cstate,
+                                     self.dout, self.dstate],
                                     feed_dict={self.rawBatch: inBatch})
             printt(cout); printt(cstate)
+            printt(dout); printt(dstate)
+            print(tf.trainable_variables())
 
     def __buildLoss(self): pass
 
@@ -80,7 +91,7 @@ def test():
         [ 2, 0, 1, 0 ]
     ]
     model.evalInputs(batch)
-    model.evalCoding(batch)
+    model.evalNetwork(batch)
 
 if __name__ == '__main__':
     test()
